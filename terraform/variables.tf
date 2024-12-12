1f0924b538710f3a09eebb4de832b58d9d41ae34
RafaGll <rafaelgonzalezllamas2001@gmail.com>
2024-12-12 13:55:42 +0100

    prueba

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,6 @@
 variable "ibmcloud_api_key" {
   description = "API key para autenticar con IBM Cloud."
   type        = string
-  default = "ngznEKwz87tO6VWwNDtYGApkpsWE3FYEwuw_ES1LT4j2"
   sensitive   = true
 }
 
